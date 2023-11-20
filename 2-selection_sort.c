e8f9dd9255f0e8082769fd06d13c091c2a6226db
Mussa Milanzi <viewholder74@gmail.com>
2023-11-20 10:37:39 -0800

    Task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,35 +1,36 @@
 #include "sort.h"
 
 /**
- * selection_sort: a function that sorts/separate an array of integers in ascending order
+ * selection_sort - a function that sorts/separate
+ * an array of integers in ascending order
  * @array: a list of numbers
  * @size: the size of the array
  *
  */
- void selection_sort(int *array, size_t size)
+
+void selection_sort(int *array, size_t size)
 {
-	size_t i, position;
-	int swap, flag = 0, tempo
+	size_t i, pos, tmp;
+	int swap, flag = 0;
 
 	if (array == NULL)
 		return;
-	for (i - 0, i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		flag = 0;
-		tempo = i;
-		for (position = i + 1; position < size; position++)
+		tmp = i;
+		for (pos = i + 1; pos < size; pos++)
 		{
-			if (array[empo] > array[position])
+			if (array[tmp] > array[pos])
 			{
-				tempo = position;
+				tmp = pos;
 				flag = flag + 1;
 			}
 		}
-		swap = array[i]
-		array[i] = array[tempo];
-		array[tempo] = swap;
+		swap = array[i];
+		array[i] = array[tmp];
+		array[tmp] = swap;
 		if (flag != 0)
 			print_array(array, size);
-	
 	}
 }
